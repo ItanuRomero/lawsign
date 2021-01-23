9986f903ac377095469fe03ec9675ba8bef58df2
Leon Hatori <leonhatori@gmail.com>
2021-01-23 12:09:59 -0300

    Include files for uploading files to app

---
+++ upload/upload.js
@@ -0,0 +1,17 @@
+// Backend
+import formidable from 'formidable';
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+export default async (req, res) => {
+  const form = new formidable.IncomingForm();
+  form.uploadDir = "./";
+  form.keepExtensions = true;
+  form.parse(req, (err, fields, files) => {
+    console.log(err, fields, files);
+  });
+};
